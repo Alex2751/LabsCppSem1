9cd7a6c582640c566abf6f21ef03a13d2614a7e3
Alex2751 <allex.masalitin@gmail.com>
2014-08-01 15:45:43 +0300

    Sem1 Lab1 Var5

---
+++ Sem1_Lab1_Var4/Sem1_Lab1_Var4/Main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	const double  a = - 0.5, b = 1.7, t = 0.44, e = 2.7183;
+
+	cout << "a = " << a << ", b = " << b << ", t = " << t << '\n' << '\n';
+
+	double l = pow(e, - b * t);
+	double m = sin(a * t + b);
+	double n = pow(fabs(b * t + a), 0.5);
+	
+	double y = l * m - n;
+	
+	cout << "y = " << y << '\n' << '\n';
+
+	double k = a * pow(t, 2) * cos(2 * t);
+	
+	double s = b * sin(k) - 1;
+
+	cout << "s = " << s << '\n';
+
+	cout << '\n';
+	system("pause");
+	return 0;
+}

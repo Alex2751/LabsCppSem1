10a64f9a49ca8f3eedbc2344bf3ca868b2c58993
Alex2751 <allex.masalitin@gmail.com>
2014-08-01 12:01:24 +0300

    Lab1 Var2, 4

---
+++ Sem1_Lab1_Var3/Sem1_Lab1_Var3/Main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	const double pi = 3.1416;
+
+	double R, r, h;
+
+	cout << "Input R, r, h\n";
+
+	cout << "R: ";
+	cin >> R;
+	cout << "r: ";
+	cin >> r;
+	cout << "h: ";
+	cin >> h;
+
+	double a = pow(h, 2) + (R - r);
+	double l = pow(a, 0.5);
+	
+	double S = pi * (R + r) * l + pi * pow(R, 2) + pi * pow(r, 2);
+	
+	cout << "S = " << S << "\n";
+
+	double V = (pi / 3) * (pow(R, 2) + pow(r, 2) + R * r) * h;
+
+	cout << "V = " << V << "\n";
+
+	cout << "\n";
+	system("pause");
+	return 0;
+}
